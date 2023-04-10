7400803ab4efc2111b74d4fee3d67caf43887cab
Jeff Chaney <113415594+Damien-88@users.noreply.github.com>
2023-04-10 13:05:02 -0700

    Add project files.

---
+++ Battleship/Battleship.py
@@ -0,0 +1,16 @@
+class Player:
+
+    def __init__(self, new_name):
+        self.name = new_name
+        self.selection = "None"
+
+title = """
+BBBBBB    AAA   TTTTTTTTTTTTTTT L       EEEEEEE   SSSSS  H     H IIIIIII PPPPPP
+B     B  A   A     T       T    L       E        S     S H     H    I    P     P
+B     B A     A    T       T    L       E        S       H     H    I    P     P
+BBBBBB  AAAAAAA    T       T    L       EEEEE     SSSSS  HHHHHHH    I    PPPPPP
+B     B A     A    T       T    L       E              S H     H    I    P    
+B     B A     A    T       T    L       E        S     S H     H    I    P
+BBBBBB  A     A    T       T    LLLLLLL EEEEEEE   SSSSS  H     H IIIIIII P
+\n\n
+"""
