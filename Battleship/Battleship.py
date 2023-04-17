c9c42df21ee09d2eb5df92fd6954ce560cf53f6f
Jeff Chaney <113415594+Damien-88@users.noreply.github.com>
2023-04-17 16:14:34 -0700

    Refactored code to streamline process and minimize bugs.

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -1,9 +1,26 @@
+import pandas as pd
+
+# Class that handles user interaction with each other and the board.;
 class Player:
 
     def __init__(self, new_name):
         
         self.name = new_name
+        
+        # Stores the attack coordinates;
         self.target = "00"
+        
+        # Stores the locations the user places each piece;
+        self.location_dict = {"Carrier": [], "Battleship": [], "Cruiser": [], "Submarine": [], "Destroyer": []}
+        
+        # Stores a list of spaces that contain a piece;
+        self.locations = []
+
+        # Piece names and sizes;
+        self.pieces = ("Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer")
+        self.sizes = (5, 4, 3, 3, 2)
+        
+        # List of available targets with sub lists of each row;
         self.available_targets = [
             ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"],
             ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"],
@@ -17,26 +34,277 @@ class Player:
             ["J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10"]
             ]
 
-    def attack(self, new_target):
+    # Method for selecting an available target to attack
+    def attack(self):
 
-        self.target = new_target.capitalize()
-        available = False
         index = 0
+        available = False
 
-        for row in range(0, len(self.available_targets)):
-            for space in range(0, len(self.available_targets[row])):
-                if self.available_targets[row][space] == self.target:
-                    index = row
+        while not available:
+            # Take in coordinates for attack.
+            coordinates = input("\n" + first.name + " Enter the coordinates for your attack: ")
+            self.target = coordinates.capitalize()
+            
+            # Loop through available targets to check if available
+            for sublist in self.available_targets:
+                if self.target in sublist: 
+                    # Remove from available targets
+                    index = self.available_targets.index(sublist)
+                    self.available_targets[index].remove(self.target)
                     available = True
-                    
-        if available:
-            self.available_targets[index].remove(self.target)
-        else:
-            print("Invalid target. Please enter an available target\n")
-            print("Available targets: \n")
-            for i in self.available_targets:
-                print(i, "\n")
 
+                    return self.target
+                    
+            if not available: # Show available. Continue loop
+                print("Invalid target. Please enter an available target\n")
+                print("Available targets: \n")
+                for i in self.available_targets:
+                    print(i, "\n")
+
+    # Method for selecting locations for each piece
+    def choose(self):
+
+        # Loop through each piece
+        for x in range(0, len(self.pieces)):
+
+            check = True
+
+            # Loop to allow user to select a new position if selection is invalid
+            while check:
+                # Lists to store the Row Letter and Column #, toggle availability
+                char = []
+                num = []
+                handy = True
+
+                # Asks for number of spaces oppropriate for indicated piece
+                piece = input(
+                    "\n" + self.name + " enter " + 
+                    str(self.sizes[x]) + " spaces for your " + 
+                    self.pieces[x] + ": "
+                    )
+                piece = piece.upper()
+                piece = piece.split(",")
+                for item in range(0, len(piece)):
+                    piece[item] = piece[item].strip()
+                
+                # Verify the correct number of spaces was chosen
+                if len(piece) == self.sizes[x]:
+                    # Loop to separate & append Row & Col lists with each selection
+                    for p in piece:
+                        char.append(p[0])
+                        num.append(p[1:])
+                        # Loop through locations to check if space has been taken.
+                        for t in self.locations:
+                            if p in t:
+                                print("\nPieces Overlap!\n\n")
+                                handy = False
+                else:
+                    if len(piece) < self.sizes[x]:
+                        print("\nNot Enough Spaces Selected! Ensure entries are separated by commas.\n\n")
+                    else:
+                        print("\nToo Many Spaces Selected!\n\n")
+                    handy = False
+
+                # Actions if all chosen spaces are available.
+                if handy:
+                    # Check if selection is Vertical. 
+                    inquire = pd.unique(char)
+                    if len(inquire) == len(char):
+                        inquire = pd.unique(num)
+                        # Verify selection is Linear. Place piece. Exit loop
+                        if len(inquire) == 1:
+                            self.placement(self.pieces[x], piece)
+                            self.locations.append(piece)
+                            check = False
+                        # If Not Linear, ouput message and continue Loop
+                        else:
+                            print("\nInvalid entry! Ensure selection is a linear path.\n\n")
+                    # Check if selection is Horizontal
+                    elif len(inquire) == 1:
+                        inquire = pd.unique(num)
+                        # Verify selection is Linear. Place piece. Exit loop
+                        if len(inquire) == len(num):
+                            self.placement(self.pieces[x], piece)
+                            self.locations.append(piece)
+                            check = False
+                        # If Not Linear, ouput message and continue Loop
+                        else:
+                            print("\nInvalid entry! Ensure selection is a linear path.\n\n")
+                    # If Neither, ouput message and continue Loop
+                    else:
+                        print("\nInvalid entry! Ensure selection is a linear path.\n\n")
+    
+    # Method for placing pieces in the dictionary
+    def placement(self, new_piece, new_location):
+        for p in self.pieces:
+            if p == new_piece:
+                self.location_dict[new_piece] = new_location
+                break
+
+    # Method for removing location selections.
+    def reset_choices(self):
+        self.location_dict = {"Carrier": [], "Battleship": [], "Cruiser": [], "Submarine": [], "Destroyer": []}
+        self.locations = []
+        
+# Class that handles activity on the board
+class Board:
+
+    # Method taking in Player Objects and setting Attributes
+    def __init__(self, new_player, other_player):
+
+        # Sets which player is Attacking & which is Defending
+        self.attacker = new_player.name
+        self.defender = other_player.name
+
+        # List containing Current Layout
+        self.panel_list = [[], [], [], [], [], [], [], [], [], []]
+
+        # Visualization of Current Layout of indicated Player Side
+        self.header = "\n" + self.defender + "'s Side!\n\n"
+        self.layout = ""
+        
+        # Import Defender Piece Locations
+        self.hit_dict = other_player.location_dict
+        self.hit_list = other_player.locations
+
+        # Import Attacker Target
+        self.aim = ""
+
+        # Lists holding Remaining Spaces for each Piece
+        self.carrier = self.hit_dict["Carrier"]
+        self.battleship = self.hit_dict["Battleship"]
+        self.cruiser = self.hit_dict["Cruiser"]
+        self.submarine = self.hit_dict["Submarine"]
+        self.destroyer = self.hit_dict["Destroyer"]
+        
+        # List of Sunk Pieces
+        self.downed = []
+
+    # Method for Default Board layout
+    def panel(self):
+
+        list_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        list_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        for letter in range(0, len(list_letters)):
+            for number in list_numbers:
+                self.panel_list[letter].append(list_letters[letter] + str(number))
+
+        self.mark_panel()
+    
+    # Method for Updating Board Display
+    def mark_panel(self):
+
+        for l in self.panel_list:
+            self.layout += "\n\n"
+            for i in l:
+                self.layout += i + "  "
+    
+    # Method verifying Hit or Miss
+    def hit_miss(self, new_target):
+
+        self.aim = new_target
+
+        # Loop trough list of piece locations.
+        for loc in self.hit_list:
+            # Check if Piece is in Target location
+            if self.aim in loc:
+                # If it is Remove Space from hit_list.
+                self.hit_list.pop(loc.index(self.aim))
+                print("Hit")
+                break
+            else:
+                print("Miss")
+
+            # Find coinsiding Piece Remove Space from it's list.
+            # Check if any Spaces are left for Piece. Call Sank if none
+            if loc == 0:
+                self.carrier.remove(self.aim)
+                if len(self.carrier) == 0:
+                    self.sank("Carrier")
+            elif loc == 1:
+                self.battleship.remove(self.aim)
+                if len(self.battleship) == 0:
+                    self.sank("Battleship")
+            elif loc == 2:
+                self.cruiser.remove(self.aim)
+                if len(self.cruiser) == 0:
+                    self.sank("Cruiser")
+            elif loc == 3:
+                self.submarine.remove(self.aim)
+                if len(self.submarine) == 0:
+                    self.sank("Submarine")
+            elif loc == 4:
+                self.destroyer.remove(self.aim)
+                if len(self.destroyer) == 0:
+                    self.sank("Destroyer")
+    
+    # Method for Updating Board Layout with Affirmative Hits
+    def blowed_up(self):
+
+        # Variable for Hit indication
+        hit = "X"
+
+        # Find Board Locationand Update Layout
+        if self.aim[0] == "A":
+            self.panel_list[0][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "B":
+            self.panel_list[1][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "C":
+            self.panel_list[2][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "D":
+            self.panel_list[3][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "E":
+            self.panel_list[4][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "F":
+            self.panel_list[5][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "G":
+            self.panel_list[6][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "H":
+            self.panel_list[7][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "I":
+            self.panel_list[8][int(self.aim[1:])-1] = hit
+        elif self.aim[0] == "J":
+            self.panel_list[9][int(self.aim[1:])-1] = hit
+        
+        # Update Display
+        self.mark_panel()
+ 
+    # Method for handling Sunken Piece
+    def sank(self, new_piece):
+
+            if new_piece == "Carrier":
+                self.downed.append("Carrier")
+                print("\n" + self.attacker + " sunk " + self.defender + "'s Carrier!\n\n")
+            elif new_piece == "Battleship":
+                self.downed.append("Battleship")
+                print("\n" + self.attacker + " sunk " + self.defender + "'s Battleship!\n\n")
+            elif new_piece == "Cruiser":
+                self.downed.append("Cruiser")
+                print("\n" + self.attacker + " sunk " + self.defender + "'s Cruiser!\n\n")
+            elif new_piece == "Submarine":
+                self.downed.append("Submarine")
+                print("\n" + self.attacker + " sunk " + self.defender + "'s Submarine!\n\n")
+            elif new_piece == "Destroyer":
+                self.downed.append("Destroyer")
+                print("\n" + self.attacker + " sunk " + self.defender + "'s Destroyer!\n\n")
+
+            if len(self.downed) == 5:
+                self.game_over()
+    
+    # Method for handling Game Over
+    def game_over(self):
+
+        if len(self.downed) > 0:
+            print("\n" + self.attacker + " WINS")
+        else:
+            print("\n" + self.defender + " WINS")
+
+        return False
+        
+
+    
+# Title Layout
 title = """
 BBBBBB    AAA   TTTTTTTTTTTTTTT L       EEEEEEE   SSSSS  H     H IIIIIII PPPPPP
 B     B  A   A     T       T    L       E        S     S H     H    I    P     P
@@ -45,5 +313,87 @@ BBBBBB  AAAAAAA    T       T    L       EEEEE     SSSSS  HHHHHHH    I    PPPPPP
 B     B A     A    T       T    L       E              S H     H    I    P    
 B     B A     A    T       T    L       E        S     S H     H    I    P
 BBBBBB  A     A    T       T    LLLLLLL EEEEEEE   SSSSS  H     H IIIIIII P
-\n\n
 """
+
+game_on = True
+coordinates = ""
+
+# Output Title
+print(title)
+
+# Setup First Player Object
+first = Player("Bill")
+
+# Setup Second Player Object
+second = Player("Ted")
+
+# Set First Player's Piece Locations
+first.choose()
+
+print("\n")
+
+# Set Second Player's Piece Locations
+second.choose()
+
+# Setup First Player Board
+board_1 = Board(first, second)
+board_1.panel()
+
+# Setup Second Player Board
+board_2 = Board(second, first)
+board_2.panel()
+
+# Turn Loop for Gamplay
+while game_on:
+     
+    # Display Defending Board
+    print(board_1.layout)
+
+    # Check
+    print("\n", board_1.carrier)
+    print("\n", board_1.hit_list)
+
+    # First Player Attack
+    open_fire = first.attack()
+
+    # Check Contact
+    board_1.hit_miss(open_fire)
+
+    #Check
+    print("\n", board_1.carrier)
+    print("\n", board_1.hit_list)
+
+    # Check for Game Over
+    game_on = board_1.game_over()
+
+    # Display Defending Board
+    print(board_2.layout)
+
+    # Check
+    print("\n", board_2.carrier)
+    print("\n", board_2.hit_list)
+
+    # Second Player Attack
+    open_fire = second.attack()
+
+    # Check Contact
+    board_2.hit_miss(open_fire)
+
+    # Check
+    print("\n", board_2.carrier)
+    print("\n", board_2.hit_list)
+
+    # Check for Game Over
+    game_on = board_2.game_over()
+
+# a1, a2, a3, a4, a5
+# b7, c7, d7, e7
+# j9, j7, j6
+# h8, h7, h6
+# f2, g2
+
+# b1, c1, d1, e1, f1
+# a10, b10, c10, d10
+# g4, g5, g6
+# f5, f6, f7
+# e5, e6
